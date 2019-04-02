52edf349ec76c1eb8e809bc9a9649d22c9aa7d60
TakMashido <takmashido2@protonmail.com>
2019-04-02 17:20:16 +0200

    Fixed CaculateOrder formula parsing

--- SHANI/src/shani/orders/CalculateOrder.java
+++ SHANI/src/shani/orders/CalculateOrder.java
@@ -1,13 +1,18 @@
 package shani.orders;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.w3c.dom.Element;
 
+import shani.Engine;
 import shani.ShaniString;
 import shani.orders.templates.KeywordOrder;
 
 public class CalculateOrder extends KeywordOrder {
+	static {
+		Engine.initialize(new String[] {"-d"});
+	}
 	private static ShaniString calculationResoultMessage=ShaniString.loadString("orders.CalculateOrder.calculationResoultMessage");
 	private static ShaniString calculationFailedMessage=ShaniString.loadString("orders.CalculateOrder.calculationFailedMessage");
 	
@@ -38,11 +43,19 @@ public class CalculateOrder extends KeywordOrder {
 		class Value{										//Creating special object for it will make easier adding multichar operations(e.g sin), complex numbers
 			double value;
 			Value(double val){value=val;}
+			
+			public String toString() {
+				return Double.toString(value);
+			}
 		}
 		class Operation{
 			char type;
 			Operation(char type){
 				this.type=type;
+			}
+			
+			public String toString() {
+				return Character.toString(type);
 			}
 		}
 		expresion=expresion.replaceAll("\\s", "");
@@ -61,16 +74,13 @@ public class CalculateOrder extends KeywordOrder {
 			}
 		}
 		
-		Object el2=elements.get(0);
-		for(int i=0;i<elements.size()-1;i++) {				//add negative numbers and fraction
+		System.out.println(Arrays.toString(elements.toArray()));
+		
+		Object el2=elements.get(0);														//Parse '.' and ','
+		for(int i=0;i<elements.size()-1;i++) {
 			Object el=el2;
 			el2=elements.get(i+1);
-			if(el instanceof Operation&&((Operation) el).type=='-') {
-				if(el2 instanceof Value) {
-					((Value) el2).value*=-1;
-					elements.remove(i--);
-				}	
-			} else if(el instanceof Value) {
+			if(el instanceof Value) {
 				if(el2 instanceof Operation&&(((Operation) el2).type==','||((Operation) el2).type=='.')) {
 					if(i+2==elements.size())elements.remove(i+1);
 					Object el3=elements.get(i+2);
@@ -82,14 +92,28 @@ public class CalculateOrder extends KeywordOrder {
 							divider*=10;
 						}
 						val=(int)((Value)el3).value;
-						if(((Value) el).value>0)((Value) el).value+=val/divider;
-						else ((Value) el).value-=val/divider;
+						((Value) el).value+=val/divider;
 						elements.remove(i+2);
 						elements.remove(i+1);
-					} else throw new Exception();				//illegal use of ',' or '.'
+					} else throw new Exception("Illegal use of ',' or '.'");				//Illegal use of ',' or '.'
 				}
 			}
 		}
+		
+		System.out.println(Arrays.toString(elements.toArray()));
+		
+		el2=elements.get(0);														//parse '-'
+		for(int i=0;i<elements.size()-1;i++) {
+			Object el=el2;
+			el2=elements.get(i+1);
+			if(el instanceof Operation&&((Operation) el).type=='-') {
+				if(el2 instanceof Value) {
+					((Value) el2).value*=-1;
+					elements.remove(i--);
+				}
+			}
+		}
+		
 		
 		Object el1=null;							//Parse pow
 		el2=elements.get(0);
@@ -97,7 +121,7 @@ public class CalculateOrder extends KeywordOrder {
 			Object el=el2;
 			el2=elements.get(i+1);
 			if(el instanceof Operation&&((Operation) el).type=='^') {
-				if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception();					//Illega use of '^'
+				if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception("Illega use of '^'");					//Illega use of '^'
 				((Value)el1).value=Math.pow(((Value)el1).value, ((Value)el2).value);
 				elements.remove(i);
 				elements.remove(i);
@@ -105,7 +129,7 @@ public class CalculateOrder extends KeywordOrder {
 			}
 			el1=el;
 		}
-		if((el1=elements.get(elements.size()-1))instanceof Operation&&((Operation)el1).type=='^') throw new Exception();		//Illega use of '^'
+		if((el1=elements.get(elements.size()-1))instanceof Operation&&((Operation)el1).type=='^') throw new Exception("Illega use of '^'");		//Illega use of '^'
 		
 		el2=elements.get(0);							//Parse * and /
 		for(int i=0;i<elements.size()-1;i++) {
@@ -113,13 +137,13 @@ public class CalculateOrder extends KeywordOrder {
 			el2=elements.get(i+1);
 			if(el instanceof Operation) {
 				if(((Operation) el).type=='*') {
-					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception();					//Illega use of '*'
+					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception("//Illega use of '*'");					//Illega use of '*'
 					((Value)el1).value*=((Value)el2).value;
 					elements.remove(i);
 					elements.remove(i);
 					i-=3;
 				} else if(((Operation) el).type=='/') {
-					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception();					//Illega use of '/'
+					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception("Illega use of '/'");					//Illega use of '/'
 					((Value)el1).value/=((Value)el2).value;
 					elements.remove(i);
 					elements.remove(i);
@@ -128,7 +152,7 @@ public class CalculateOrder extends KeywordOrder {
 			}
 			el1=el;
 		}
-		if((el1=elements.get(elements.size()-1))instanceof Operation&&((Operation)el1).type=='^')throw new Exception();			//Illega use of '*' or '/'
+		if((el1=elements.get(elements.size()-1))instanceof Operation&&((Operation)el1).type=='^')throw new Exception("Illega use of '*' or '/'");			//Illega use of '*' or '/'
 		
 		el2=elements.get(0);							//Parse + and /
 		for(int i=0;i<elements.size()-1;i++) {
@@ -136,13 +160,13 @@ public class CalculateOrder extends KeywordOrder {
 			el2=elements.get(i+1);
 			if(el instanceof Operation) {
 				if(((Operation) el).type=='+') {
-					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception();					//Illega use of '+'
+					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception("Illegal use of '+'");					//Illega use of '+'
 					((Value)el1).value+=((Value)el2).value;
 					elements.remove(i);
 					elements.remove(i);
 					i-=3;
 				} else if(((Operation) el).type=='-') {
-					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception();					//Illega use of '-'
+					if(i==0||!(el1 instanceof Value)||!(el2 instanceof Value))throw new Exception("Illegal use of '-'");					//Illega use of '-'
 					((Value)el1).value-=((Value)el2).value;
 					elements.remove(i);
 					elements.remove(i);
