7b85746df35bbe81c1cac6228de07c0322d88052
Tak Mashido <takmashido2@protonmail.com>
2021-09-21 12:02:41 +0200

    Improved Executable interface.

--- takMashido.shani/src/takMashido/shani/orders/Executable.java
+++ takMashido.shani/src/takMashido/shani/orders/Executable.java
@@ -1,17 +1,18 @@
 package takMashido.shani.orders;
 
+import takMashido.shani.core.Cost;
+
 /**
  *Ready to execute Action with cost between execute pattern and input ShaniString.
- * @author TakMashido
- */
+ * @author TakMashido*/
 public final class Executable{
-	/**Cost between input and action invoke pattern.*/
-	public final short cost;
-	/***/
-	public final short importanceBias;
+	/**Cost between this executable Action and Intend it's response to.*/
+	public final Cost cost;
+
 	/**{@link Action} which this Executable represent*/
 	public final Action action;
-	private boolean succesful=false;
+	/**Says if execution of stored action was successful. It's value returned by Action::execute()*/
+	private boolean successful =false;
 	
 	/**Creates Executable.
 	 * @param action Action to be stored.
@@ -27,27 +28,30 @@ public final class Executable{
 	 */
 	public Executable(Action action, short cost, short importanceBias) {
 		this.action=action;
-		this.cost=(short)(cost+action.getCost());
-		this.importanceBias=(short)(importanceBias+action.getImportanceBias());
+		this.cost=new Cost((short)(cost+action.getCost()), (short)(importanceBias+action.getImportanceBias()));
 	}
-	
+	public Executable(Action action, Cost cost) {
+		this.action=action;
+		this.cost=cost;
+	}
+
 	/**
 	 * Executes stored action.
 	 * @see Action#execute()
 	 */
 	public void execute() {
-		succesful=action.execute();
+		successful =action.execute();
 	}
 	
 	/**Check if stored action was successfully executed.
 	 * @return If action successfully executed or false if not executed.
 	 */
-	public boolean isSuccesful() {
-		return succesful;
+	public boolean isSuccessful() {
+		return successful;
 	}
 	
 	@Override
 	public String toString(){
-		return action.toString()+" costs:"+cost+":"+importanceBias;
+		return action.toString()+" "+cost.toString();
 	}
 }
